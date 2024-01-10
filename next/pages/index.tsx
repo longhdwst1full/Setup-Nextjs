73089ce5b0eb3d6ef3ea21dc450d43b2dd42545f
dev.fintwin <hoanglonggg356@gmail.com>
2024-01-10 09:50:07 +0700

    update

---
+++ next/pages/index.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import LayoutMain from "@/common/components/LayoutMain";
+
+
+export default function Home() {
+  return (
+    <LayoutMain>
+      <div>dfđf</div>
+    </LayoutMain>
+  );
+}
